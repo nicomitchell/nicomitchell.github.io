2bb17c89631347ec8e3b9a484e4e32bec2ba91ae
nicomitchell <namitc02@louisville.edu>
2017-09-22 13:12:41 -0400

    fixed musicplayer script

--- musicPlayer.js
+++ musicPlayer.js
@@ -3,7 +3,11 @@ var playing = true;
 function toggleAudio() {
     if (playing) {
         audio.pause();
+        playing = false;
+        document.getElementById("musicButton").innerHTML = "Music off";
     } else {
         audio.play();
+        playing = true;
+        document.getElementById("musicButton").innerHTML = "Music on";
     }
 }
